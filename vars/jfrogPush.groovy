4f432a1956a86e2d4c9020ee8bc31b0c07d83e81
Adarshats2396 <155351395+Adarshats2396@users.noreply.github.com>
2024-01-30 12:35:55 +0530

    Update jfrogPush.groovy

--- vars/jfrogPush.groovy
+++ vars/jfrogPush.groovy
@@ -1,5 +1,5 @@
 def call() {
-     withCredentials([usernameColonPassword(credentialsId: 'guthub', variable: 'fis')]) {
-           sh 'docker login -u adarshgowda676@gmail.com slkgroup.jfrog.io -p ${fis}'
-           sh "docker push ${Image_Tag}"
+     withCredentials([string(credentialsId: 'fis_new', variable: 'fis')]) {
+                       sh 'docker login -u adarshgowda676@gmail.com adarsha.jfrog.io -p ${fis}'
+                       sh "docker push ${Image_Tag}"
 }
