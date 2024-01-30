1c899da194a3b598a5d7a66976c3c44712e9ccfe
Adarshats2396 <155351395+Adarshats2396@users.noreply.github.com>
2024-01-30 13:01:23 +0530

    Update jfrogPush.groovy

--- vars/jfrogPush.groovy
+++ vars/jfrogPush.groovy
@@ -1,4 +1,4 @@
 def call() {
-    sh 'docker login -u adarshgowda676@gmail.com adarsha.jfrog.io -p ${fis}'
-    sh "docker push ${Image_Tag}"
+   sh 'docker login -u adarshgowda676@gmail.com adarsha.jfrog.io -p ${fis}'
+   sh "docker push ${Image_Tag}"
 }
